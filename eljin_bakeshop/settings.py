9e5186769fa2ddfb5c5b21fc547425e8a3bb0668
markranny <77148720+markranny@users.noreply.github.com>
2025-05-17 07:53:40 +0800

    r11

--- eljin_bakeshop/settings.py
+++ eljin_bakeshop/settings.py
@@ -132,18 +132,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-""" STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-] """
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'bakeshop/static'),  
 ]
+
+""" STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'bakeshop/static'),  
+] """
 
 # Media files
 MEDIA_URL = '/media/'
